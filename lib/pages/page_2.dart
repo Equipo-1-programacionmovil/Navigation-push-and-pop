93b61ff1fbf4a4ec577334b4b150f7607b649915
Julio <juliocgp30@gmail.com>
2024-05-08 12:53:36 -0600

    Plantilla Base

--- lib/pages/page_2.dart
+++ lib/pages/page_2.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 // ignore: camel_case_types
 class page2 extends StatelessWidget {
   const page2({super.key});
-
+//
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
