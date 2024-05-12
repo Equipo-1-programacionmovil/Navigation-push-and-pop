11cb31638f0a9d4d50309614a116b2c9254acb66
Julio <juliocgp30@gmail.com>
2024-05-11 23:16:50 -0600

    Terminado app

--- lib/pages/page_3.dart
+++ lib/pages/page_3.dart
@@ -46,7 +46,7 @@ class page3 extends StatelessWidget {
                       context,
                       MaterialPageRoute(builder: (context) => const page1()),
                     );
-                    //
+                    //final
                   },
                 ),
               ],
